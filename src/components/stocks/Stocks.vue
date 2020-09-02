be49ebbd6108c842100848f1e2aa45c4111b8b53
yujialves <yujialves@i.softbank.jp>
2020-09-02 13:59:26 +0900

    Add a buy button

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-stock v-for="(stock) in stocks" :key="stock.id"></app-stock>
+    <app-stock v-for="(stock) in stocks" :key="stock.id" :stock="stock"></app-stock>
   </div>
 </template>
 
