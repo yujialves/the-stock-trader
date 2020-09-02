9bea2b0f18779db55ca039f386615c6c16a9d828
yujialves <yujialves@i.softbank.jp>
2020-09-02 15:57:59 +0900

    Set up vue-resource

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import { routes } from "./routes";
 import store from "./store/store";
+import VueResource from "vue-resource";
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = "ROOT_URL";
 
 Vue.filter("currency", (value) => {
   return "$" + value.toLocaleString();
